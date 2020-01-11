d3a256f1b20ff41a994698be878576984277e454
saranyavelu <saranvelu90@gmail.com>
2020-01-11 11:56:30 +0530

    CBL added

--- dataset.py
+++ dataset.py
@@ -13,3 +13,14 @@ def Data_PBL():
 
     df1.to_csv("Dataset_PBLandPrL.csv", header = True, index = False)
 
+def Data_CBL():
+    size = 5
+    start = 1
+    end = 11
+
+    df2 = pandas.DataFrame(list(randomdata.Rand(start, end, size)), columns = ['Technology change', 'Political influence', 'Cost of Education', 'Man Power', 'Money Inflation'])
+
+    df2['Instructional Methodology'] = 'CBL'
+
+    df2.to_csv("Dataset_CBL.csv", header = True, index = False)
+
